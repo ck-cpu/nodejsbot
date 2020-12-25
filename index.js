d5deb63d1b7e6b35da27cb11a3b342523a738981
ck-cpu <kevinsh525@gmail.com>
2020-12-26 02:00:01 +0900

    discord bot

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ client.on('message', (message) => {
 
 client.on('message', (message) => {
   if(message.content === '$샘플 목록') {
-    message.channel.send('현재 샘플은\n없습니다. (총 0개)');
+    message.channel.send('현재 샘플은\n라이츄(이)가 있습니다. (총 1개)');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '$샘플 라이츄') {
+    message.channel.send('라이츄 @기합의띠\n성격: 겁쟁이\n특성 : 피뢰침\n개체값 : 공격 제외 5v\n노력치 : h 4 cs 252\n!10만볼트\n!볼트체인지\n!파도타기\n!나쁜음모');
   }
 });
 
