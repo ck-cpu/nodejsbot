1a8a6a019b3f839d5da7f52b427a8f4f9111f493
ck-cpu <kevinsh525@gmail.com>
2020-12-26 01:54:24 +0900

    discord bot

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
-  if(message.content === '$샘플 브리가론') {
-    message.channel.send('브리가론 @악키열매\n성격: 장난꾸러기\n노력치: H252 B252 D4\n특성: 심록 or 방탄\n!광합성\n!벌크업\n!우드해머\n!니들가드');
-  }
-});
-
-client.on('message', (message) => {
   if(message.content === '$봇 초대링크') {
     message.channel.send('https://discord.com/oauth2/authorize?client_id=791929491924189204&scope=bot');
   }
@@ -21,7 +15,7 @@ client.on('message', (message) => {
 
 client.on('message', (message) => {
   if(message.content === '$샘플 목록') {
-    message.channel.send('현재 샘플은\n브리가론이(가) 있습니다. (총 1개)');
+    message.channel.send('현재 샘플은\n없습니다. (총 0개)');
   }
 });
 
