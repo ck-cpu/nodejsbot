7931ef025cf1c026d52677d3eb7d0e94b683cb8f
ck-cpu <kevinsh525@gmail.com>
2020-12-26 12:41:36 +0900

    discord bot

--- index.js
+++ index.js
@@ -43,4 +43,48 @@ client.on('message', (message) => {
   }
 });
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'NzkxOTI5NDkxOTI0MTg5MjA0.X-WT0A.1293grkKh3Y0FoV18cj5TN1mO-U';
+const welcomeChannelName = "안녕하세요";
+const byeChannelName = "안녕히가세요";
+const welcomeChannelComment = "어서오세요.";
+const byeChannelComment = "안녕히가세요.";
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on('message', (message) => {
+  if(message.author.bot) return;
+
+  if(message.content == 'ping') {
+    return message.reply('pong');
+  }
+
+  if(message.content.startsWith('$전체공지')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('$전체공지'.length);
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(`<@${message.author.id}> ${contents}`);
+      });
+  
+      return message.reply('공지를 전송했습니다.');
+    } else {
+      return message.reply('채널에서 실행해주세요.');
+    }
+  }
+});
+
+function checkPermission(message) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+    return true;
+  } else {
+    return false;
+  }
+}
+
 client.login(token);
