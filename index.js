8f20780d3b29f90c111a714c2c94f33bb848a838
ck-cpu <kevinsh525@gmail.com>
2020-12-26 13:22:32 +0900

    discord bot

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on('message', (message) => {
 
 client.on('message', (message) => {
   if(message.content === '$현재 이벤트굴') {
-    message.channel.send('현재 이벤트 레이드 굴은 32번째 이벤트굴이며 대표적으로 딜리버드, 알로라 식스테일, 미로치 알로라 식스테일 (2%)가 등장합니다. (기간: 12월 25일~12월 27일)\nr자세한 정보는 https://www.serebii.net/swordshield/maxraidbattles/eventden-christmas2020.shtml 에서 참고해주세요!');
+    message.channel.send('현재 이벤트 레이드 굴은 32번째 이벤트굴이며 대표적으로 딜리버드, 알로라 식스테일, 미로치 알로라 식스테일 (2%)가 등장합니다. (기간: 12월 25일~12월 27일)\n자세한 정보는 https://www.serebii.net/swordshield/maxraidbattles/eventden-christmas2020.shtml 에서 참고해주세요!');
   }
 });
 
