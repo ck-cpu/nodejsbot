dc67cce34e9b08c51d826dbcd6327cc61809fecd
ck-cpu <kevinsh525@gmail.com>
2020-12-26 11:59:42 +0900

    discord bot

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on('message', (message) => {
 
 client.on('message', (message) => {
   if(message.content === '$샘플 목록') {
-    message.channel.send('현재 샘플은\n라이츄, 핫삼(이)가 있습니다. (총 1개)');
+    message.channel.send('현재 샘플은\n라이츄, 핫삼(이)가 있습니다. (총 2개)');
   }
 });
 
@@ -25,14 +25,20 @@ client.on('message', (message) => {
 });
 
 client.on('message', (message) => {
-  if(message.content === '$야') {
-    message.channel.send('힘들어');
+  if(message.content === '$샘플 핫삼') {
+    message.channel.send('핫삼 @구애머리띠 \n성격 : 고집\n특성 : 테크니션\n개체값 : 6v\n노력치 H 236 A 252 S 20\n!유턴\n!불릿펀치\n!더블윙\n!엄청난힘');
   }
 });
 
 client.on('message', (message) => {
-  if(message.content === '$샘플 핫삼') {
-    message.channel.send('핫삼 @구애머리띠 \n성격 : 고집\n특성 : 테크니션\n개체값 : 6v\n노력치 H 236 A 252 S 20\n!유턴\n!불릿펀치\n!더블윙\n!엄청난힘');
+  if(message.content === '$도움') {
+    message.channel.send('쿠키샘플봇에는 이 명령어가 존재합니다.\n$샘플 목록: 현재 등록 되어있는 샘플을 알려줍니다.\n$샘플 (포켓몬 이름): 그 포켓몬의 샘플을 알려줍니다. 쓰실 때 $샘플 목록을 확인하시고 쓰시기 바랍니다.\n$봇 초대링크: 쿠키샘플봇의 초대링크로 자기 서버에 쿠키샘플봇을 추가할 수 있습니다.\n$도움 봇의 명령어를 알려줍니다.\n$봇 의견: 봇의 대한 의견을 추가 하는 방법을 알려줍니다');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '$봇 의견') {
+    message.channel.send('쿠키샘플봇의 대한 의견은 https://discord.gg/xhPEgPenAb 이 서버의 #봇-의견 채널에서 작성해주세요.');
   }
 });
 
