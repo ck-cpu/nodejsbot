2ecbe7204a29f4c3cddc767fb70e633082727ea5
ck-cpu <kevinsh525@gmail.com>
2020-12-27 13:02:25 +0900

    discord bot

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ client.on('ready', () => {
   client.user.setPresence({ game: { name: '$도움을 쳐보세요!' }, status: 'online' })
 });
 
+
+
+if(message.content.startsWith('!전체공지')) {
+  if(checkPermission(message)) return
+  if(message.member != null) { // 채널에서 공지 쓸 때
+    let contents = message.content.slice('!전체공지'.length);
+    message.member.guild.members.array().forEach(x => {
+      if(x.user.bot) return;
+      x.user.send(`<@${message.author.id}> ${contents}`);
+    });
+
+    return message.reply('공지를 전송했습니다.');
+  } else {
+    return message.reply('채널에서 실행해주세요.');
+  }
+}
+;
+
+function checkPermission(message) {
+if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+  message.channel.send(`<@${message.author.id}> 명령어를 수행할 관리자 권한을 소지하고 있지않습니다.`)
+  return true;
+} else {
+  return false;
+}
+}
+
 client.on('message', (message) => {
   if(message.content === '$성격 노력') {
     message.channel.send('```변함 없음```');
