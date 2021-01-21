8d16a1d9f81f3a63d9290e10d4534d90b25b2aff
ck-cpu <kevinsh525@gmail.com>
2021-01-21 17:05:18 +0900

    discord bot

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on('message', (message) => {
 if (message.content == '임베드') {
   const embed = new Discord.MessageEmbed() 
       .setTitle('임베드 테스트') 
-      .setDescription('안녕하세요') 
+      .setDescription('https://media.discordapp.net/attachments/794941121431076885/801452569821577256/pokemon.png') 
       .setColor('#00ffff') 
   message.channel.send(embed)
 }
