fb94ea0994ea3e5b394115a1d913c2467cd9f623
ck-cpu <kevinsh525@gmail.com>
2021-01-21 20:40:39 +0900

    d

--- index.js
+++ index.js
@@ -28,9 +28,21 @@ client.on('message', (message) => {
     if (message.content == '쿠키 도움말') {
       const embed = new Discord.MessageEmbed() 
           .setTitle('쿠키봇 명령어 리스트입니다!') 
-          .setDescription('아직 명령어를 추가하고있어요..조금만 기다려주세요..!') 
+          .setDescription('쿠키 도움말: 쿠키봇에 명령어를 알려줘요!') 
           .setColor('YELLOW') 
       message.channel.send(embed)
     }
     })
+
+    client.on('message', (message) => {
+      if (message.content == '쿠키 좋아하는거?') {
+        const embed = new Discord.MessageEmbed() 
+            .setTitle('저는 포켓몬 도치마론을 좋아해요!!') 
+            .setDescription('으악...넘 귀엽지않나요....?') 
+            .setColor('GREEN') 
+            .setFooter(`${message.author.username}님은 뭘 좋아하시나요..?ㅎㅎ`)
+        message.channel.send(embed)
+      }
+      })
+
 client.login(token);
