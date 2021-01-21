8166d663d05698633e124ccf1ec939ff8d46b136
ck-cpu <kevinsh525@gmail.com>
2021-01-21 22:23:18 +0900

    discord

--- index.js
+++ index.js
@@ -69,11 +69,11 @@ client.on('message', (message) => {
           client.on('message', (message) => {
             if (message.content == '아르세우스 나와랏!') {
               const embed = new Discord.MessageEmbed() 
-                  .setTitle('A wild pokemon has appeared') 
-                  .setDescription('Use p!catch <pokemon name> to catch it')
+                  .setTitle('야생 포켓몬이 나타났다!') 
+                  .setDescription('p!catch <포켓몬 이름>을 통해서 잡으세요!')
                   embed.setImage("https://images-ext-2.discordapp.net/external/Ewqdsanlw6fe9P1VwEJ0BENIbFFVM_zcDkZh6IX46qo/https/pokecord.xyz%3A443/pokepic/2c477ce5c6765f3dc912a89f96f6125c%3A65d8d43a05bd7cc7b15e1e21a8508771")
                   embed.setColor('BLACK')
-                  .setFooter(`The next pokemon will replace this one! •2021.01.21.`)
+                  .setFooter(`다음 포켓몬이 나타나면 잡을 수 없습니다! •2021.01.21.`)
               message.channel.send(embed)
             }
             })
