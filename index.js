8474e04c1d82f0eb1bbaf496217b652cff886e48
ck-cpu <kevinsh525@gmail.com>
2021-01-21 16:54:13 +0900

    discord bot

--- index.js
+++ index.js
@@ -12,4 +12,24 @@ client.on('message', (message) => {
   }
 });
 
+client.on('message', (message) => {
+  if(message.content === '$초대링크') {
+    message.channel.send('https://discord.com/api/oauth2/authorize?client_id=791929491924189204&permissions=8&scope=bot');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '$a등급') {
+    message.channel.send('```이벤트는 현재 기획 중입니다.```');
+  }
+});
+
+if (message.content == '임베드') {
+  const embed = new Discord.MessageEmbed() 
+      .setTitle('임베드 테스트') 
+      .setDescription('안녕하세요') 
+      .setColor('#00ffff') 
+  message.channel.send(embed)
+}
+
 client.login(token);
