5493fb83e924d58e93c859d469d1b479ff6bf4b6
ck-cpu <kevinsh525@gmail.com>
2021-01-22 16:57:48 +0900

    d

--- index.js
+++ index.js
@@ -115,7 +115,48 @@ client.on('message', (message) => {
                       message.reply(`${addedFood} 를 추가했습니다!`);
                   };
               });
-                
-                
 
-client.login(token);
+              client.on('message', message => {
+    
+                let foods = ["족발", "피자", "치킨"]
+            
+                if (message.content.startsWith(`!이로치`)) {
+                    let rand = Math.floor(Math.random() * foods.length);
+                    message.reply(`${foods[rand]}`);
+                };
+            
+                if (message.content.startsWith(`!싫어추가`)) {
+                    let addedFood = message.content.split(`!ㅋ추가 `)[1];
+                    foods.push(addedFood);
+                    message.reply(`${addedFood} 를 추가했습니다!`);
+                };
+            });
+            const request = require("request")
+
+            if (message.content === '!코로나') {
+              let url = "https://apiv2.corona-live.com/stats.json"
+              request(url, (error, response, body) => {
+                  let overview = JSON.parse(response.body).overview;
+                  overview = {
+                      total_confirmed_person: overview.confirmed[0], // 총 확진자수
+                      yesterday_confirmed_person: overview.confirmed[1], // 어제 확진자수
+              
+                      current_confirmed_person: overview.current[0], // 현재 확진자수
+                      current_confirmed_person_diff: overview.current[1], // diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+                  }
+              
+                  let embed = new Discord.MessageEmbed()
+                  embed.setTitle('코로나 라이브 홈페이지')
+                  embed.setURL('https://corona-live.com')
+                  embed.setColor('#FF8000')
+                  embed.setDescription('코로나 정보입니다')
+                  embed.addField(`대한민국 총 확진자 수`, `${overview.total_confirmed_person}명`, true)
+                  embed.addField(`어제 확진자 수`, overview.yesterday_confirmed_person + `명`, true)
+                  embed.addField(`오늘 확진자 수`, overview.current_confirmed_person + `명`, true)
+                  // embed.addField(`오늘 어제지금시간   -   현재지금시간의 확진자`, overview.current_confirmed_person_diff + `명`, true)
+                  message.channel.send(embed)
+              
+                })
+              }
+              
+            client.login(token);
