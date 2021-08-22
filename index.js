b7b86c4358a7cf62d20e024294b4f64cfbb23aea
ck-cpu <kevinsh525@gmail.com>
2021-08-22 23:15:24 +0900

    d

--- index.js
+++ index.js
@@ -47,9 +47,23 @@ client.on('message', (message) => {
                 .setTitle('**Raid announcement**') 
                 .setDescription('New raid will start in 15 minutes!\nWill you defeat **Shinx?**')
                 .setImage('http://play.pokemonshowdown.com/sprites/ani-shiny/shinx.gif')
-                .setColor('#000000') 
+                .setColor('#616264') 
+                .setFooter('오늘 오후 11:42')
             message.channel.send(embed)
           }
           })
+
+          client.on('message', (message) => {
+            if (message.content == '%레이드 레지스틸 이로치') {
+              const embed = new Discord.MessageEmbed() 
+                  .setTitle('**Raid announcement**') 
+                  .setDescription('New raid will start in 15 minutes!\nWill you defeat **Registeel?**')
+                  .setImage('http://play.pokemonshowdown.com/sprites/ani-shiny/registeel.gif')
+                  .setColor('#616264') 
+                  .setFooter('오늘 오후 11:42')
+              message.channel.send(embed)
+            }
+            })
+
 
   client.login(token);
